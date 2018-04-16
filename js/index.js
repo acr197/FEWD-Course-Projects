281440eb47e5f21f3461331935644612b0109002
Andrew Ryan <andryan@publicisgroupe.net>
2018-04-16 09:38:43 -0400

    Animation

--- js/index.js
+++ js/index.js
@@ -225,21 +225,7 @@ $('.pers-top-section').ready(function(){
 
 //SCROLL PAGE ANIMATE VIEWPORT
 
-(function($, win) {
-  $.fn.inViewport = function(cb) {
-     return this.each(function(i,el){
-       function visPx(){
-         var H = $(this).height(),
-             r = el.getBoundingClientRect(), t=r.top, b=r.bottom;
-         return cb.call(el, Math.max(0, t>0? H-t : (b<H?b:H)));  
-       } visPx();
-       $(win).on("resize scroll", visPx);
-     });
-  };
-}(jQuery, window));
-
-
-
-$(".fun-facts-section").inViewport(function(px){
-    if(px) $(this).addClass("triggeredCSS3") ;
+$(function() {
+  AOS.init({delay:600, duration:500});
 });
+
